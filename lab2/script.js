56718a76b3217865c99ccb82f608767f3b117595
Anton <anivanov1987788990012@gmail.com>
2024-11-18 12:23:44 +0300

    lab2

--- lab2/script.js
+++ lab2/script.js
@@ -1,13 +1,24 @@
-let personalMovieDB = {
+const personalMovieDB = {
     privat: false,
     movies: {
-        "1+1": 8,
-        "Основатель": 9,
-        "Игрок": 6
+        "1+1": 9,
+        "Титаник": 10,
+        "Зеленая книга": 9,
     }
-};
-
-function displayMoviesDB(db) {
-    if (db.privat) return;
 }
 
+function showMovies(){
+    if (!personalMovieDB.privat){
+        const tableContainer = document.getElementById('content')
+        let tableHTML = '<table><tr><th>Название фильма</th><th>Оценка</th><tr>';
+
+        for (let movie in personalMovieDB.movies) {
+            tableHTML += `<tr><td>${movie}</td><td>${personalMovieDB.movies[movie]}</td></tr>`;
+        }
+
+        tableHTML += '</table>';
+        tableContainer.innerHTML = tableHTML;
+    }
+}
+
+showMovies();
