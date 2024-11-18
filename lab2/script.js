32063b2d19e6ad64b17f5ec2fb41b457dec829e0
Anton <anivanov1987788990012@gmail.com>
2024-11-18 12:13:34 +0300

    lab 2

---
+++ lab2/script.js
@@ -0,0 +1,13 @@
+let personalMovieDB = {
+    privat: false,
+    movies: {
+        "1+1": 8,
+        "Основатель": 9,
+        "Игрок": 6
+    }
+};
+
+function displayMoviesDB(db) {
+    if (db.privat) return;
+}
+
